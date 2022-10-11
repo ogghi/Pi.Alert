e6a079d8599c77a397e51c50506c77d676376fcb
David Schmidt <david.schmidt>
2022-10-11 14:10:25 +0200

    Fixing the tar creation script etc

--- tar/create_tar.sh
+++ tar/create_tar.sh
@@ -8,18 +8,18 @@
 #  Puche 2021        schmidt@linux.com        GNU GPLv3
 # ------------------------------------------------------------------------------
 
-PIALERT_DEV_PATH=/home/david/workdir/Pi.Alert/
+PIALERT_DEV_PATH=/home/david/workdir/
 cd $PIALERT_DEV_PATH
 pwd
-PIALERT_VERSION=`awk '$1=="VERSION" { print $3 }' pialert/config/version.conf | tr -d \'`
+PIALERT_VERSION=`awk '$1=="VERSION" { print $3 }' Pi.Alert/config/version.conf | tr -d \'`
 
 # ------------------------------------------------------------------------------
-ls -l pialert/tar/pialert*.tar
-tar tvf pialert/tar/pialert_latest.tar | wc -l
-rm pialert/tar/pialert_*.tar
+ls -l Pi.Alert/tar/pialert*.tar
+tar tvf Pi.Alert/tar/pialert_latest.tar | wc -l
+rm Pi.Alert/tar/pialert_*.tar
 
 # ------------------------------------------------------------------------------
-tar cvf pialert/tar/pialert_$PIALERT_VERSION.tar --exclude="pialert/tar" --exclude="pialert/.git" pialert | wc -l
+tar cvf Pi.Alert/tar/pialert_$PIALERT_VERSION.tar --exclude="Pi.Alert/tar" --exclude="Pi.Alert/.git" pialert | wc -l
 
-ln -s pialert_$PIALERT_VERSION.tar pialert/tar/pialert_latest.tar
-ls -l pialert/tar/pialert*.tar
+ln -s pialert_$PIALERT_VERSION.tar Pi.Alert/tar/pialert_latest.tar
+ls -l Pi.Alert/tar/pialert*.tar
